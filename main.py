13968462c0e22d04d0128628b6b15a15f3e005d6
wx02 <100297402+wanxin2002@users.noreply.github.com>
2024-09-17 13:36:16 +0800

    Extracted details of restaurant with matching country code (task 1)

--- main.py
+++ main.py
@@ -13,9 +13,40 @@ def extract_json_data(url):
         print(f"Failed to fetch data. Status code: {response.status_code}")
     return data
 
-restaurant_data = extract_json_data(restaurant_url)[0] # Get the 1st element (dictionary) of the list
+restaurant_data = extract_json_data(restaurant_url)
 country_data = pd.read_excel("./data/Country-Code.xlsx")
 
+# To match country code & get country
+country_codes = country_data['Country Code'].tolist()
+country_dict = country_data.set_index('Country Code')['Country'].to_dict()
 
+# Get restaurant details and events
+restaurant_details = []
 
+for i in range(5): # for testing -> range(len(restaurant_data))
+
+    restaurants_list = restaurant_data[i]['restaurants']
+
+    for restaurant in restaurants_list:
+        details = restaurant['restaurant']
+        
+        # Task 1: Only include restaurants with matching country code
+        country_id =  details['location']['country_id']
+        if country_id in country_codes: 
+            event = details.get('zomato_events', None)
+            restaurant_details.append({
+                'Restaurant Id': details['id'],
+                'Restaurant Name': details['name'],
+                'Country': country_dict[country_id], 
+                'City': details['location']['city'],
+                'User Rating Votes': int(details['user_rating']['votes']),
+                'User Aggregate Rating': float(details['user_rating']['aggregate_rating']),
+                'Cuisines': details['cuisines'],
+                'Event Data Extraction': event,
+                'Number of Events': len(event)
+            })
+
+# Save outputs
+restaurant_details_df = pd.DataFrame(restaurant_details)
+restaurant_details_df.to_csv('./output/restaurant_details.csv', index=False)
 
